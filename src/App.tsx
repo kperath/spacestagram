425c6025188611ea2e812bb7f632158649004a92
Kunju Perath <kperath@gmail.com>
2022-01-09 23:14:52 -0500

    Added app title

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Card from './components/Card';
 function App() {
   return (
     <div className="bg-gray-100 h-screen">
+      <h1 className="app-title text-6xl md:text-8xl p-6 text-center">Spacestagram</h1>
+      <h6 className="app-title text-center mb-2">Powered by Nasa 🚀</h6>
       <Card/>
     </div>
   );
