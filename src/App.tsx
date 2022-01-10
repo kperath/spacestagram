e244a272d21121246e5fa14248992464f9487204
Kunju Perath <kperath@gmail.com>
2022-01-10 00:39:01 -0500

    using dayjs to get last 3 months of data
    built in javascript Date API too messy and results in harder to read code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import Card from "./components/Card";
+import dayjs from "dayjs";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [posts, setPosts] = useState<dayjs.Dayjs[]>([]);
+
+  useEffect(() => {
+    const last3Months: dayjs.Dayjs[] = [];
+    for (let i = 1; i <= 9; i++) {
+      last3Months.push(dayjs().add(-i, "day"));
+    }
+    setPosts(last3Months);
+  }, []);
+
   return (
     <div className="bg-gray-100 h-screen">
       <h1 className="app-title text-6xl md:text-8xl p-6 text-center">
         Spacestagram
       </h1>
       <h6 className="app-title text-center mb-2">Powered by Nasa 🚀</h6>
+
       <Card />
+      {posts.forEach((p) => console.log(p))}
     </div>
   );
 }
