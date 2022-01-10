cd1ed2cd522589e40e986179e697aeeea9d1197c
Kunju Perath <kperath@gmail.com>
2022-01-10 01:25:29 -0500

    fixed card styling and added date to card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,23 +1,27 @@
 import { useState } from "react";
 import nasa from "../nasa.jpg";
 
-function Card() {
+interface props {
+  dateTime: string;
+}
+
+const Card = ({ dateTime }: props) => {
   const [liked, setLiked] = useState(false);
 
   return (
-    <div className="container mx-auto p-4 flex justify-center">
-      <div className="max-w-sm rounded-lg overflow-hidden shadow-lg">
-        <img className="w-full" src={nasa} alt="Sunset in the mountains" />
-        <div className="px-6 pt-4 pb-2 bg-white">
-          <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-          <p className="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.
-          </p>
+    <div className="my-4 mx-2 max-w-sm rounded-lg overflow-hidden shadow-lg bg-white">
+      <img className="w-full" src={nasa} alt="Sunset in the mountains" />
+      <div className="px-6 pt-4 pb-2">
+        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+        <p className="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
+        </p>
+        <div className="flex align-bottom mt-2 mb-1 justify-between">
           <button
             onClick={() => setLiked((prevState) => !prevState)}
-            className={`border-2 border-transparent px-2 -ml-2 mt-2 rounded-full transform transition duration-300 scale-110 hover:scale-125 ${
+            className={`border-2 border-transparent px-2 -ml-2 rounded-full transform transition duration-300 scale-110 hover:scale-125 ${
               liked ? "fill-red-500" : "fill-gray-400"
             }`}
           >
@@ -33,10 +37,13 @@ function Card() {
               <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
             </svg>
           </button>
+          <span className="bg-gray-200 rounded-full px-3 -mr-4 py-1 text-sm font-semibold text-gray-700 mt-1">
+            {dateTime}
+          </span>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Card;
